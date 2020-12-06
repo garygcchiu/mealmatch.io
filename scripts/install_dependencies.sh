ae461148424d76b6b2702b8fce50b766a895c41f
Gary <gary.gc.chiu@gmail.com>
2020-12-06 13:06:12 -0500

    source bashrc intead

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source /home/ec2-user/.bashrc
 
 npm -v
 node -v
